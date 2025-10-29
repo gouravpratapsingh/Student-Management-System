781aa958dce203fd11ee470f9348354ea506e3c5
gouravpratapsingh <singhgouravpratap295@gmail.com>
2025-10-29 20:19:41 +0530

    update controller

--- src/main/java/student/controller/WebController.java
+++ src/main/java/student/controller/WebController.java
@@ -37,7 +37,7 @@ public class WebController {
         return "home";
     }
 
-    @GetMapping("/home")
+    @GetMapping("/Student-Mgmt")
     public String backToHome(){
         return "home";
     }
